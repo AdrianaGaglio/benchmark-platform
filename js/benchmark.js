3a572548157aff25a22fc4522da53a24aedc3777
Riccardo Santilli <riccardosantilli@live.it>
2024-09-03 16:50:22 +0200

    WIP - check correct answers

--- js/benchmark.js
+++ js/benchmark.js
@@ -1,6 +1,9 @@
 // indice di selezione della domanda
 let index = 0;
 
+const correctAnswers = 0;
+const wrongAnswers = 0;
+
 const getQuestions = (level) => {
   // genero array di domande in base al livello scelto
   const questionsArray = questions.filter((question) => question.difficulty === level);
@@ -39,11 +42,43 @@ const questionsLoop = (array, index) => {
       tempAnswersArray.splice(randomIndex, 1);
       answerWrap.appendChild(answer);
       answersContainer.appendChild(answerWrap);
+      const risposta = array[index].correct_answers;
+      console.log(risposta);
       // evidenzio la risposta selezionata
       answer.onclick = () => {
         answer.classList.add("highlighted");
+        if (answer.innerText === array[index].correct_answers) {
+          console.log("giusto!");
+        } else {
+          console.log(risposta);
+        }
       };
     }
+  }
+};
+
+const chartColor = () => {
+  //funzione per cambiare colori e testi in base al risultato del quiz
+  const circle = document.getElementById("outside-circle");
+  const correctText = document.querySelector("#correct-answers h2");
+  const wrongText = document.querySelector("#wrong-answers h2");
+  // const correctAnswers = 90.9 + "%";
+  // const wrongAnswers = 9.1 + "%";
+  correctText.innerHTML = correctAnswers;
+  wrongText.innerHTML = wrongAnswers;
+  circle.style.background = `conic-gradient(#c1158b 0% ${wrongAnswers}%, #00ffff ${wrongAnswers}% ${correctAnswers}%)`;
+  failedExams(wrongAnswers);
+};
+
+const failedExams = (wrongAnswers) => {
+  // funzione per cambiare testo in caso di esito negativo
+  if (wrongAnswers >= 50) {
+    const textInCircle = document.getElementById("center-text");
+    textInCircle.innerHTML = "";
+    const examFailed = document.createElement("p");
+    examFailed.className = "failed";
+    examFailed.innerHTML = "Sorry! <p>You failed the quiz.</p>";
+    textInCircle.appendChild(examFailed);
   }
 };
 
@@ -79,4 +114,5 @@ window.onload = () => {
       });
     }
   };
+  chartColor();
 };
