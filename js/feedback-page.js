04f62fdd29eb112792282526ccbbf2278ace7b45
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-09-04 12:15:12 +0200

    wip - adding modal to feedback page

--- js/feedback-page.js
+++ js/feedback-page.js
@@ -1,52 +1,64 @@
-let stars = Array.from(document.querySelectorAll('.star'));
-console.log(stars);
+let stars = document.querySelectorAll(".star");
 
 function resetStars() {
-    stars.forEach(function (star) {
-        star.classList.remove('selected');
-    });
+  stars.forEach(function (star) {
+    star.classList.remove("selected");
+  });
 }
 
 function setStarsUpTo(index) {
-    resetStars();
-    for (let j = 0; j <= index; j++) {
-        stars[j].classList.add("selected");
-    }
+  resetStars();
+  for (let j = 0; j <= index; j++) {
+    stars[j].classList.add("selected");
+  }
 }
 
 window.onload = () => {
-    let selectedIndex = -1;
+  let selectedIndex = -1;
 
-    for (let i = 0; i < stars.length; i++) {
-        stars[i].onmouseover = function () {
-            if (selectedIndex === -1) {
-                setStarsUpTo(i);
-            }
-        };
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].onmouseover = function () {
+      if (selectedIndex === -1) {
+        setStarsUpTo(i);
+      }
+    };
 
-        stars[i].onmouseout = function () {
-            if (selectedIndex === -1) {
-                resetStars();
-            }
-        };
+    stars[i].onmouseout = function () {
+      if (selectedIndex === -1) {
+        resetStars();
+      }
+    };
 
-        stars[i].onclick = function () {
-            selectedIndex = i;
-            setStarsUpTo(i);
-        };
-    }
+    stars[i].onclick = function () {
+      selectedIndex = i;
+      setStarsUpTo(i);
+      document.querySelector("#comment-input").disabled = false;
+    };
+  }
 };
 
+// document.addEventListener("DOMContentLoaded", function () {
+//     const btn = document.querySelector(".default-btn");
 
+//     btn.addEventListener("click", function () {
+//         btn.classList.toggle("active-btn");
+//     });
+// });
 
+const feedbackButton = document.querySelector(".default-btn");
+const feedbackText = document.querySelector("#comment-input");
 
-console.dir(document.querySelector(".star"))
+feedbackText.oninput = function (event) {
+  const selectedStars = document.querySelectorAll(".selected");
 
+  if (feedbackText.value !== "" && selectedStars.length > 0) {
+    feedbackButton.classList.add("active-btn");
+  }
+};
 
-document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.querySelector(".default-btn");
+const feedbackForm = document.querySelector("form");
 
-    btn.addEventListener("click", function () {
-        btn.classList.toggle("active-btn");
-    });
-});
+feedbackForm.onsubmit = function (event) {
+  event.preventDefault();
+  if (feedbackText.value === "") alert("You must leave a feedback before submit");
+};
